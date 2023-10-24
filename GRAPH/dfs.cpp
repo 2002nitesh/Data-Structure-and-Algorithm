d4961355c2d7b7bd230269d25ca2c0eec520e475
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2023-10-24 17:18:42 +0000

    dfs

--- GRAPH/dfs.cpp
+++ GRAPH/dfs.cpp
@@ -1,6 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void dfsOfGraph(int node, vector<int> adj[], int vis[], vector<int> &dfs)
+{
+    vis[node] = 1;
+    dfs.push_back(node);
+    // traverse all its neighbours
+    for (auto it : adj[node])
+    {
+        // if the neighbour is not visited
+        if (!vis[it])
+        {
+            dfsOfGraph(it, adj, vis, dfs);
+        }
+    }
+}
+
+int main()
+{
+
     return 0;
 }
