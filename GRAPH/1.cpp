6a3bfd1105bb4d160132c1deebe2ace1b180b859
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2023-10-25 16:17:45 +0000

    number of provinces

---
+++ GRAPH/1.cpp
@@ -0,0 +1,49 @@
+// Number of Provinces
+// Problem Statement: Given an undirected graph with V vertices. We say two vertices u and v belong to a single province if there is a path from u to v or v to u. Your task is to find the number of provinces.
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  private: 
+    // dfs traversal function 
+    void dfs(int node, vector<int> adjLs[], int vis[]) {
+        // mark the more as visited
+        vis[node] = 1; 
+        for(auto it: adjLs[node]) {
+            if(!vis[it]) {
+                dfs(it, adjLs, vis); 
+            }
+        }
+    }
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        vector<int> adjLs[V]; 
+        
+        // to change adjacency matrix to list 
+        for(int i = 0;i<V;i++) {
+            for(int j = 0;j<V;j++) {
+                // self nodes are not considered
+                if(adj[i][j] == 1 && i != j) {
+                    adjLs[i].push_back(j); 
+                }
+            }
+        }
+        int vis[200] = {0};  // we have assume that V<=200
+        int cnt = 0; 
+        for(int i = 0;i<V;i++) {
+            // if the node is not visited
+            if(!vis[i]) {
+                // counter to count the number of provinces 
+                cnt++;
+               dfs(i, adjLs, vis); 
+            }
+        }
+        return cnt; 
+        
+    }
+};
+
+int main(){
+    
+    return 0;
+}
