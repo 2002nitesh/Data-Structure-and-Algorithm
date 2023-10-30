6ab5336feb18db3462437b5a2e615347eb647c1c
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2023-10-30 14:50:09 +0000

    number of islands

---
+++ GRAPH/11.cpp
@@ -0,0 +1,11 @@
+// G-30 : Word Ladder-II
+// Given two distinct words startWord and targetWord, and a list denoting wordList of unique words of equal lengths. Find all shortest transformation sequence(s) from startWord to targetWord. You can return them in any order possible.
+
+// In this problem statement, we need to keep the following conditions in mind:
+
+// A word can only consist of lowercase characters.
+// Only one letter can be changed in each transformation.
+// Each transformed word must exist in the wordList including the targetWord.
+// startWord may or may not be part of the wordList.
+// Return an empty list if there is no such transformation sequence.
+
